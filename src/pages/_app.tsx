9eb20ab9619292837e4e0a453a5632641c6cc013
Mehmet Cinar <mcinarpro@gmail.com>
2022-04-27 23:39:57 +0200

    fix recoil warnings

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,15 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { AuthProvider } from '../hooks/useAuth'
 import { RecoilRoot } from 'recoil'
+import { memoize } from 'lodash'
+
+// ignore in-browser next/js recoil warnings until its fixed.
+const mutedConsole = memoize((console: any) => ({
+  ...console,
+  warn: (...args: any) =>
+    args[0].includes('Duplicate atom key') ? null : console.warn(...args),
+}))
+global.console = mutedConsole(global.console)
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -30,15 +30,17 @@ const Home = ({
   topRated,
   trendingNow,
 }: Props) => {
-  
   const { loading } = useAuth()
   const showModal = useRecoilValue(modalState)
 
-  
   if (loading) return null
 
   return (
-    <div className="relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh]">
+    <div
+      className={`relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh] ${
+        showModal && '!h-screen overflow-hidden'
+      }`}
+    >
       <Head>
         <title>Home - Netflix</title>
         <link rel="icon" href="/favicon.ico" />
